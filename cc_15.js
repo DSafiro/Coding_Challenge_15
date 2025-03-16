28db08818265bf2b577410fe1ce5df7b031bfc51
Dylan <safirodylan@gmail.com>
2025-03-16 00:09:33 -0400

    Task 1 - Base Structure Setup

---
+++ cc_15.js
@@ -0,0 +1,4 @@
+// Task 1: Base Structure Setup
+const riskDashboard = document.getElementById("riskDashboard"); // Selects risk dashboard container using getElementById
+
+console.log("Risk Dashboard Loaded"); // Outputs risk dashboard loaded
